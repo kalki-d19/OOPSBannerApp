e7f70b6228d94ef8605289703ed3846dae55e793
dkalki-19 <dkalkicdhandapani@gmail.com>
2026-01-29 12:56:48 +0530

    [kalki] Added:UC4:Render OOPS as Banner using String Array

--- src/bridgelabz/OOPsBannerApp.java
+++ src/bridgelabz/OOPsBannerApp.java
@@ -3,7 +3,22 @@ package bridgelabz;
 public class OOPsBannerApp {
 
 	public static void main(String[] args) {
-		System.out.println("Bridgelabz");
+		// Create an empty String array to hold 7 lines
+        String[] bannerLines = new String[7];
+        
+        // Populate each line using String.join()
+        bannerLines[0] = String.join("", "  ***   ", "  ***   ", " ******* ", "  ****** ");
+        bannerLines[1] = String.join("", " *   *  ", " *   *  ", " *     * ", " *      *");
+        bannerLines[2] = String.join("", "*     * ", "*     * ", "*       *", "*        ");
+        bannerLines[3] = String.join("", "*     * ", "*     * ", "*******  ", " ******* ");
+        bannerLines[4] = String.join("", "*     * ", "*     * ", "*        ", "        *");
+        bannerLines[5] = String.join("", " *   *  ", " *   *  ", "*        ", "*      * ");
+        bannerLines[6] = String.join("", "  ***   ", "  ***   ", "*        ", " ****** ");
+        
+        // Use enhanced for loop to print each line
+        for (String line : bannerLines) {
+            System.out.println(line);
+        }
 	}
 
 }
