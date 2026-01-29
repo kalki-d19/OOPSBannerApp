5adc90cd7c1d50ad5f7795dae04c80f0d3443598
dkalki-19 <dkalkicdhandapani@gmail.com>
2026-01-29 13:39:42 +0530

    [Kalki] Added:UC8:Use Map for Character Patterns and Render via Function

--- src/bridgelabz/OOPsBannerApp.java
+++ src/bridgelabz/OOPsBannerApp.java
@@ -1,9 +1,35 @@
 package bridgelabz;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class OOPsBannerApp {
 
-	public static void main(String[] args) {
-		System.out.println("Bridgelabz");
-	}
+	 public static void main(String[] args) {
+	        Map<Character, String[]> patternMap = buildPatternMap();
+	        displayBanner("OOPS", patternMap);
+	    }
+
+	    static Map<Character, String[]> buildPatternMap() {
+
+	        Map<Character, String[]> map = new HashMap<>();
+
+	        map.put('O', new String[]{" ***** "," *   * "," *   * "," *   * "," *   * "," *   * "," ***** "});
+	        map.put('P', new String[]{" ****  "," *   * "," *   * "," ****  "," *     "," *     "," *     "});
+	        map.put('S', new String[]{" ***** "," *     "," *     "," ***** ","     * ","     * "," ***** "});
+
+	        return map;
+	    }
+
+	    static void displayBanner(String word, Map<Character, String[]> map) {
+
+	        for (int i = 0; i < 7; i++) {
+	            StringBuilder line = new StringBuilder();
+	            for (char ch : word.toCharArray()) {
+	                line.append(map.get(ch)[i]).append(" ");
+	            }
+	            System.out.println(line);
+	        }
+	    }
 
 }
