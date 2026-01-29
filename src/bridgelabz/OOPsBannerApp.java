ebfed8d21c63675a73480b9f97658184c5962374
dkalki-19 <dkalkicdhandapani@gmail.com>
2026-01-29 13:21:54 +0530

    [Kalki] Added:UC6:public static void main(String[] args) {

--- src/bridgelabz/OOPsBannerApp.java
+++ src/bridgelabz/OOPsBannerApp.java
@@ -3,7 +3,36 @@ package bridgelabz;
 public class OOPsBannerApp {
 
 	public static void main(String[] args) {
-		System.out.println("Bridgelabz");
-	}
+
+        String[] banner = buildBanner();
+
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+
+    static String[] buildBanner() {
+        return new String[]{
+                String.join(" ", getO()[0], getO()[0], getP()[0], getS()[0]),
+                String.join(" ", getO()[1], getO()[1], getP()[1], getS()[1]),
+                String.join(" ", getO()[2], getO()[2], getP()[2], getS()[2]),
+                String.join(" ", getO()[3], getO()[3], getP()[3], getS()[3]),
+                String.join(" ", getO()[4], getO()[4], getP()[4], getS()[4]),
+                String.join(" ", getO()[5], getO()[5], getP()[5], getS()[5]),
+                String.join(" ", getO()[6], getO()[6], getP()[6], getS()[6])
+        };
+    }
+
+    static String[] getO() {
+        return new String[]{" ***** ", " *   * ", " *   * ", " *   * ", " *   * ", " *   * ", " ***** "};
+    }
+
+    static String[] getP() {
+        return new String[]{" ****  ", " *   * ", " *   * ", " ****  ", " *     ", " *     ", " *     "};
+    }
+
+    static String[] getS() {
+        return new String[]{" ***** ", " *     ", " *     ", " ***** ", "     * ", "     * ", " ***** "};
+    }
 
 }
